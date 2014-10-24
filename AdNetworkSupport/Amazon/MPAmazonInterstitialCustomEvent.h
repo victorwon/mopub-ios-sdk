49adf41f9bdf83b9206becfa9844e588a56dbc52
Victor Weng <victor.weng@gmail.com>
2014-10-24 19:15:56 -0400

    Added amazon adapters

---
+++ AdNetworkSupport/Amazon/MPAmazonInterstitialCustomEvent.h
@@ -0,0 +1,16 @@
+//
+//  MPAmazonInterstitialCustomEvent.h
+//
+//
+//  Copyright (c) 2014 Idemfactor Solutions Inc. All rights reserved.
+//
+
+#import "MPInterstitialCustomEvent.h"
+#import <AmazonAd/AmazonAdInterstitial.h>
+
+/*
+ */
+
+@interface MPAmazonInterstitialCustomEvent : MPInterstitialCustomEvent <AmazonAdInterstitialDelegate>
+
+@end
